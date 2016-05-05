9fec96d34e42588569815586c1af7d9bcec9e65d
Dera Andrian <ared09@gmail.com>
2016-05-05 20:50:33 +0300

    Add event listeners

---
+++ src/Defi/PageBundle/Form/EventListener/AddVerseFieldSubscriber.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Defi\PageBundle\Form\EventListener;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+/**
+ * Description of AddChapterFieldSubscriber
+ *
+ * @author sparrow
+ */
+class AddVerseFieldSubscriber implements EventSubscriberInterface {
+
+    private $propertyPathToBook;
+    private $em;
+
+    public function __construct($propertyPathToBook, \Doctrine\ORM\EntityManager $em) {
+        $this->propertyPathToBook = $propertyPathToBook;
+        $this->em = $em;
+    }
+
+    public static function getSubscribedEvents() {
+        return array(
+            FormEvents::PRE_SET_DATA => 'preSetData',
+            FormEvents::PRE_SUBMIT => 'preSubmit'
+        );
+    }
+
+    private function addVerseForm($form, $bookId = null, $chapterId = null, $verseStartIdx = null) {
+        $verseStart = array();
+        $verseEnd = array();
+
+        if ($bookId !== null && $chapterId !== null) {
+            $contentRepository = $this->em->getRepository('DefiCommonBundle:Content');
+            $upperBoundVerse = $contentRepository->getUpperBoundVerse($bookId, $chapterId, $verseStartIdx);
+            
+            for ($i = 1; $i <= $upperBoundVerse; $i++) {
+                $verseStart[$i] = $i;
+            }
+
+            if ($verseStartIdx) {
+                for ($i = $verseStartIdx; $i <= $upperBoundVerse; $i++) {
+                    $verseEnd[$i] = $i;
+                }
+            }
+        }
+
+        $form->add('verseStart', ChoiceType::class, array(
+            'required' => false,
+            'choices' => $verseStart
+        ));
+
+        $form->add('verseEnd', ChoiceType::class, array(
+            'required' => false,
+            'choices' => $verseEnd
+        ));
+    }
+
+    public function preSetData(FormEvent $event) {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        $bookId = null;
+        $chapterId = null;
+
+        if (null !== $data) {
+            $accessor = PropertyAccess::createPropertyAccessor();
+            $book = $accessor->getValue($data, $this->propertyPathToBook);
+            $chapter = $accessor->getValue($data, 'chapter');
+
+            if ($book) {
+                $bookId = $book->getId();
+            }
+
+            if ($chapter) {
+                $chapterId = $chapter->getId();
+            }
+        }
+
+        $this->addVerseForm($form, $bookId, $chapterId);
+    }
+
+    public function preSubmit(FormEvent $event) {
+        $data = $event->getData();
+        $form = $event->getForm();
+        $bookId = array_key_exists('book', $data) ? $data['book'] : null;
+        $chapterId = array_key_exists('chapter', $data) ? $data['chapter'] : null;
+        $verseStart = array_key_exists('verseStart', $data) ? $data['verseStart'] : null;
+
+        $this->addVerseForm($form, $bookId, $chapterId, $verseStart);
+    }
+
+}
